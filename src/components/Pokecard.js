9ca2332e66c5248fa8b82583b8c02160c1d9accb
SidAbhi <megamancodenamezerobytes@gmail.com>
2021-06-17 03:45:29 +0700

    added more data from the api

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -6,8 +6,13 @@ function Pokecard(props) {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [name, setName] = useState('');
-  const [image, setImage] = useState('');
+  const [thumbnail, setThumbnail] = useState('');
   const [id, setId] = useState(0);
+  const [primaryType, setPrimaryType] = useState('');
+  const [secondaryType, setSecondaryType] = useState('');
+  const [firstAbility, setFirstAbility] = useState('');
+  const [secondAbility, setSecondAbility] = useState('');
+  const [hiddenAbility, setHiddenAbility] = useState('');
 
   useEffect(() =>{
     fetch(api)
@@ -17,6 +22,21 @@ function Pokecard(props) {
           console.log(result)
           setIsLoaded(true);
           setName(result.name);
+          setThumbnail('https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/thumbnails/' + result.id + '.png');
+          setId(result.id);
+          setPrimaryType(result.types[0].type.name);
+          if (result.types.length === 2) {
+            setSecondaryType(result.types[1].type.name);
+          }
+          setFirstAbility(result.abilities[0].ability.name);
+          if (result.abilities.length > 1 && result.abilities[1].slot === 2) {
+            setSecondAbility(result.abilities[1].ability.name);
+          }
+          if (result.abilities.length > 1 && result.abilities[1].slot === 2) {
+            setHiddenAbility(result.abilities[2].ability.name);
+          } else if (result.abilities.length > 1 && result.abilities[1].slot === 3) {
+            setHiddenAbility(result.abilities[1].ability.name);
+          }
         },
         (error) => {
           setIsLoaded(true);
@@ -33,7 +53,14 @@ function Pokecard(props) {
   } else {
     return (
       <div className={styles.pokecard}>
-        {name}
+        <img src={thumbnail} alt={name}></img>
+        {name}-
+        {id}-
+        {primaryType}-
+        {secondaryType}-
+        {firstAbility}-
+        {secondAbility}-
+        {hiddenAbility}
       </div>
     )
   }
